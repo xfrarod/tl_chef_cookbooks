05e516683e966b53e29e42341c34de06015ae600
mons3rrat <msede25@gmail.com>
2019-01-14 12:31:27 -0600

    updating nginx

--- cookbook/custom_nginx/metadata.rb
+++ cookbook/custom_nginx/metadata.rb
@@ -7,7 +7,7 @@ long_description 'Installs/Configures custom_nginx'
 version '0.1.0'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
-depends 'nginx', '~> 8.1.2'
+depends 'nginx', '~> 9.0.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
